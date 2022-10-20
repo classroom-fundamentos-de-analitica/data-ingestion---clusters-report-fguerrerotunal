abfc0e08097084719e380c45b6c3b0beb52a254b
fguerrerotunal <53582759+fguerrerotunal@users.noreply.github.com>
2022-10-19 19:45:30 -0500

    intento 1

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,39 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
-
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
+    
+    import re
+    
+    with open('clusters_report.txt') as cosaMaluca:
+        table = cosaMaluca.readlines()
+    
+    
+    header = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+    nueva = []
+    fila = []
+    
+    table = table[4:]
+    
+    for line in table:
+        
+        #primeras
+        if re.match("^ +\d", line):
+            cluster, cantidad, porcentaje, *palabras = line.split()
+            palabras = " ".join(palabras[1:])
+            fila = [int(cluster), int(cantidad), float(porcentaje.replace(",",".")), palabras]
+        
+        #intermedias
+        elif re.match("^ +\w", line):
+            palabras = line.split()
+            palabras = " ".join(palabras)
+            fila[3] += " " + palabras
+            
+        #separador
+        else:
+            fila[3] = fila[3].replace(".", "")
+            nueva.append(fila)
+    
+    df = pd.DataFrame(nueva,columns=header)
+    
     return df
